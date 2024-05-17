bb7d529c6a27824c631a3f4534f6d08d9bbbc945
João Pedro <jpdebem@gmail.com>
2024-05-17 09:48:40 -0300

    Criando repositorio

---
+++ BlzCrd.Backend/Program.cs
@@ -0,0 +1,27 @@
+using BlzCrd.Backend.Data;
+using BlzCrd.Backend.Entities;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<AppDbContext>(x => { x.UseSqlite("DataSource=app.db;Cache=Shared"); });
+
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/v1/categories", async (AppDbContext context) =>
+{
+    var categories = await context.Categories.AsNoTracking().ToListAsync();
+    return Results.Ok(categories);
+}).Produces<List<Category>>();
+
+app.MapPost("/v1/categories", async (AppDbContext context, Category category) =>
+{
+    await context.Categories.AddAsync(category);
+    await context.SaveChangesAsync();
+
+    return Results.Created($"/v1/categories/{category.Id}", category);
+});
+
+
+app.Run();
